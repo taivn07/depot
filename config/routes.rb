9d7e5bb49afd0b8987e233d75e641e3de712481a
taivn07 <taivn08@gmail.com>
2012-04-26 11:55:41 +0900

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -74,7 +74,7 @@ Depot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'store#index'
+   root :to => 'wellcome#index'
 
   # See how all your routes lay out with "rake routes"
 
